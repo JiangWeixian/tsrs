4462f55081c432170cb20ed82af8e1aa20336a1b
JW <jiangweixian1994@gmail.com>
2025-01-05 19:17:22 +0800

    :white_check_mark:  add tomatchdir

---
+++ crates/binding_node/tests/transform.spec.ts
@@ -0,0 +1,20 @@
+import path from 'node:path'
+
+import {
+  describe,
+  expect,
+  it,
+} from 'vitest'
+import { toMatchDir } from 'vitest-extra'
+
+import { transform } from '../index.js'
+
+expect.extend({ toMatchDir })
+
+describe('optimize', () => {
+  it('barrel', async () => {
+    const root = path.join(__dirname, '../../../fixtures/package-a')
+    transform({ root, barrelPackages: ['@mui/material'] })
+    expect(path.join(root, 'dist')).toMatchDir()
+  })
+})
